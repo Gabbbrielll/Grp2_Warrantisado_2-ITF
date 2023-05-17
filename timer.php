3d5357f0dcd000e35638d3ab01c1cb188658d719
LanzGlinoga <133775814+LanzGlinoga@users.noreply.github.com>
2023-05-17 17:11:12 +0800

    Update timer.php
    buset mode

--- timer.php
+++ timer.php
@@ -6,19 +6,43 @@ date_default_timezone_set('Asia/Manila');
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get input date from user
-  $input_date = $_POST['input_date'];
+    // Get input date from user
+    $input_date = $_POST['input_date'];
 
-  // Convert input date to timestamp
-  $input_timestamp = strtotime($input_date);
+    // Convert input date to timestamp
+    $input_timestamp = strtotime($input_date);
 
-  // Calculate time difference between input date and current date in days
-  $time_diff = floor(($input_timestamp - time()) / (60 * 60 * 24));
+    // Get current date
+    $current_date = strtotime(date('Y-m-d'));
 
-  // Check if input date is exactly the 7th day from the current date
-  if ($time_diff === 7) {
-    // Output warning message
-    echo "WARNING: This date is on the 7th day from today's date!";
-  }
+    // Check if input date is the current date
+    if ($input_timestamp >= $current_date) {
+        // Output warning message
+        echo "WARNING: This date is today!";
+        
+        // Display prompt with radio form
+        echo "<form action='' method='POST'>
+            <p>Do you want to delete this warning message?</p>
+            <input type='radio' name='delete_option' value='yes' id='delete_yes' required>
+            <label for='delete_yes'>Yes</label>
+            <input type='radio' name='delete_option' value='no' id='delete_no' required>
+            <label for='delete_no'>No</label>
+            <button type='submit'>Submit</button>
+        </form>";
+
+        // Check if the delete option is selected
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_option'])) {
+            $delete_option = $_POST['delete_option'];
+            if ($delete_option === 'yes') {
+                // Perform deletion action
+                // Replace the following line with your actual code for deletion
+                // For example, if you want to delete a file, you can use unlink() function
+                // unlink('path_to_warning_message.txt');
+                echo "Warning message deleted.";
+            } else {
+                echo "Warning message retained.";
+            }
+        }
+    }
 }
 ?>
